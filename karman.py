0952a44955034a829d92b6c6e268648ca7a4cf2d
daimonie <deboer@lorentz.leidenuniv.nl>
2018-04-23 16:34:18 +0200

    Almost deleted a bunch of things.

--- karman.py
+++ karman.py
@@ -9,18 +9,11 @@ class karmanVortexSheet(latticeBoltzmann):
 	def geometry(self):
 		"""Defines the geometry, boundaries and suchlike. In the base class, this is a pipe. """
 
-		self.obstacles = np.fromfunction(lambda x, y: ((x-self.length/4)**2 + (y - self.width/2)**2) < (self.width/9)**2, (self.nodes[0], self.nodes[1]))
-
-
-		print("is there an object?")
-		for i in range(self.obstacles.shape[0]):
-			for j in range(self.obstacles.shape[1]):
-				if self.obstacles[i,j] != False:
-					print(self.obstacles[i, j])
-		sys.exit(0)
-
-
-		self.flow = np.fromfunction(lambda dd, xx, yy: (1-dd) * self.velocity * (1 + 1e-4*np.sin(yy/self.width*2*np.pi)), (2, self.nodes[0], self.nodes[1]))
+		#circular object
+		self.obstacles = np.fromfunction(lambda x, y: ((x-self.nodes[0]/4)**2 + (y - self.nodes[1]/2)**2) < (self.nodes[1]/9)**2, (self.nodes[0], self.nodes[1]))
+ 
+ 		#sinusoidal flow
+		self.flow = np.fromfunction(lambda dd, xx, yy: (1-dd) * self.velocity * (1 + 1e-4*np.sin(yy/self.nodes[1]*2*np.pi)), (2, self.nodes[0], self.nodes[1]))
 
 		#initial distributions
 		self.distributions = self.equilibrium( 1.0, self.flow)
