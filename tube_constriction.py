41b0edb55f279db7b9f59db93ef146cd1294c1e5
Daimonie <joskodeboer@gmail.com>
2018-06-06 13:20:07 +0200

    Fixed constriction

--- tube_constriction.py
+++ tube_constriction.py
@@ -6,13 +6,23 @@ from numba import jit
  
 class tubeConstriction(latticeBoltzmann):
     """ D2Q9 LB Karman Vortex Sheet class.""" 
-    def geometry(self, location):
+    def geometry(self, location, width, height):
         """Defines the geometry, boundaries and suchlike. In the base class, this is a pipe. """
 
-        if location > 1:
-        	raise ValueError("karmanVortexSheet::geometry: Location %.3f should be a fractional number < 1." % location)
         #circular object
-        self.obstacles = np.fromfunction(lambda x, y: ((x-self.nodes[0]*location)**2 + (y - self.nodes[1]/2)**2) < (self.length_scale)**2, (self.nodes[0], self.nodes[1]))
+        
+        left_horizontal = self.nodes[0]*location-width/2
+        right_horizontal =  self.nodes[0]*location+width/2
+
+        upper_vertical = self.nodes[1]/2-height/2
+        lower_vertical = self.nodes[1]/2+height/2
+
+        is_obstacle_horizontal = lambda x: (x > left_horizontal) and  (x < right_horizontal)
+        is_obstacle_vertical = lambda y:  (y < upper_vertical) or (y > lower_vertical) 
+        
+        is_obstacle = lambda x, y: is_obstacle_horizontal(x) and is_obstacle_vertical(y)
+
+        self.obstacles = np.array([[is_obstacle(y,x) for x in range(self.nodes[1])] for y in range(self.nodes[0])])
  
          #sinusoidal flow
         self.flow = np.fromfunction(lambda dd, xx, yy: (1-dd) * self.velocity * (1 + 1e-4*np.sin(yy/self.nodes[1]*2*np.pi)), (2, self.nodes[0], self.nodes[1]))
