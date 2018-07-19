702b0d72a89b8431f0005c8bd05c5b7535b0e4f7
Daimonie <joskodeboer@gmail.com>
2018-07-19 11:45:13 +0200

    Attempts at making a nicer module

---
+++ lbm/simulation.py
@@ -0,0 +1,10 @@
+import numpy as np 
+
+#simple class for lbm, based on palabos notebooks
+class lattice_boltzmann:
+	#all constants are in the config dict
+	def __init__(self, config):
+
+		#check the properties of the dict
+
+
